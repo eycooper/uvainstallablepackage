e28cb0b7a7eeb15d8d3f088b59f3d6647357a9ae
eycooper <emma.cooper@hotmail.com>
2023-02-17 10:19:08 -0500

    Added pytest and more tests to test_shared.py

--- tests/test_shared.py
+++ tests/test_shared.py
@@ -1,8 +1,26 @@
 import sys
 sys.path.append('.')
 import shared as sh
+import pytest
 
 def test_clean_string():
     test_str = " This! is      a ,test string  "
 
     assert "This is a test string" == sh.clean_string(test_str), "String <{}> not cleaned as expected".format(test_str)
+
+def test_space_compress():
+    assert sh.space_compress('word             word') == 'word word', "Failed multiple spaces"
+
+@pytest.mark.xfail
+def test_failing():
+    assert sh.space_compress('word!' ) == 'word'
+
+@pytest.mark.skip
+def test_skip():
+    assert False
+
+@pytest.mark.skipif(sys.platform == 'win32', reason = "OS runs on win32")
+def test_platform():
+    print(sys.platform)
+    print("My platform is", sys.platform)
+    assert False
